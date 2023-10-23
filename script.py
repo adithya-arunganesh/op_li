b97f3a57f8611061b36f1eec0650902460acafb5
Adithya Arunganesh <adithyaa@Adithyas-MBP.attlocal.net>
2023-10-23 18:14:15 -0500

    opening browser with webbrowser, unable to close tabs

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import subprocess
+import webbrowser
 
 def open_visual_studio_code():
     try:
@@ -6,5 +7,14 @@ def open_visual_studio_code():
     except FileNotFoundError:
         print("Visual Studio Code is not installed or not in your system's PATH.")
 
+def open_browser():
+    try:
+        browser = webbrowser.get('safari')
+        browser.open('http://google.com')
+        browser.open_new_tab('http://youtube.com')
+    except webbrowser.Error:
+        print("Browser control error")
+
 if __name__ == "__main__":
     open_visual_studio_code()
+    open_browser()
